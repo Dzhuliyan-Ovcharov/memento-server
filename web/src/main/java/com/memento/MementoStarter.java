f8dc8a6b652bb9404f334e7eca399324f611cb3e
dovcharov <djuliqn.bg@gmail.com>
2019-08-17 22:56:50 +0300

    Initial project commit

---
+++ web/src/main/java/com/memento/MementoStarter.java
@@ -0,0 +1,12 @@
+package com.memento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MementoStarter.class, args);
+    }
+}
