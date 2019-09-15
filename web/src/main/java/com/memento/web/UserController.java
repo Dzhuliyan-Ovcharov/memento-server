3043afbf0acd0a58516d742f76221da538e6d9bd
dovcharov <djuliqn.bg@gmail.com>
2019-09-15 21:46:09 +0300

    added register and login methods

--- web/src/main/java/com/memento/web/UserController.java
+++ web/src/main/java/com/memento/web/UserController.java
@@ -3,8 +3,11 @@ package com.memento.web;
 import com.memento.model.User;
 import com.memento.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -14,10 +17,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserController(final UserService userService) {
+    public UserController(final UserService userService, final BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @GetMapping(value = "/all")
@@ -27,8 +32,18 @@ public class UserController {
 
     @PostMapping(value = "/register")
     public ResponseEntity<User> register(@Valid @RequestBody final User user) {
-       return null;
+        return ResponseEntity.ok(userService.save(user));
     }
 
-    
+    @PostMapping(value = "/login")
+    public ResponseEntity<User> login(@Valid @RequestBody final User user) {
+        final User foundUser = (User) userService.loadUserByUsername(user.getUsername());
+
+        if (!bCryptPasswordEncoder.matches(user.getPassword(), foundUser.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong username or password");
+        }
+
+        return ResponseEntity.ok(foundUser);
+    }
+
 }
