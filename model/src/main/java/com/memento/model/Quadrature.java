8385cb7b38de88fa063e84274a38bec3f11f03f1
dovcharov <djuliqn.bg@gmail.com>
2019-10-01 20:41:50 +0300

    Make Quadrature with BigDecimal

--- model/src/main/java/com/memento/model/Quadrature.java
+++ model/src/main/java/com/memento/model/Quadrature.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Getter
 @Embeddable
@@ -12,22 +13,22 @@ public class Quadrature implements Serializable {
 
     private static final long serialVersionUID = -6869288107131789383L;
 
-    @Column(name = "built_up_area")
-    private final Integer builtUpArea;
+    @Column(name = "built_up_area", precision = 3, scale = 2)
+    private final BigDecimal builtUpArea;
 
-    @Column(name = "pure_area")
-    private final Integer pureArea;
+    @Column(name = "pure_area", precision = 3, scale = 2)
+    private final BigDecimal pureArea;
 
-    public Quadrature of(final Integer builtUpArea, final Integer pureArea) {
+    public Quadrature of(final BigDecimal builtUpArea, final BigDecimal pureArea) {
         return new Quadrature(builtUpArea, pureArea);
     }
 
-    private Quadrature(final Integer builtUpArea, final Integer pureArea) {
+    private Quadrature(final BigDecimal builtUpArea, final BigDecimal pureArea) {
         this.builtUpArea = builtUpArea;
         this.pureArea = pureArea;
     }
 
-    public Integer getDifference() {
-        return builtUpArea - pureArea;
+    public BigDecimal getDifference() {
+        return builtUpArea.subtract(pureArea);
     }
 }
