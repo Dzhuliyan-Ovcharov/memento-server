98ffe35c0857c8bb62123a09cd2da18c9d46e669
dovcharov <djuliqn.bg@gmail.com>
2019-12-01 20:01:57 +0200

    Blank test

---
+++ service/src/test/java/com/memento/service/impl/AdTypeServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.memento.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdTypeServiceImplTest {
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void save() {
+    }
+}
